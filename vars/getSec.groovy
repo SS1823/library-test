7265e60071f03f2bef9826ef1a2ed65996759c4e
Szymon Sobik <84793504+SS1823@users.noreply.github.com>
2021-09-02 21:42:31 +0200

    df

--- vars/getSec.groovy
+++ vars/getSec.groovy
@@ -1,12 +1,23 @@
 def genSec(String a, String b, boolean boo) {
   echo "a='$a' b='$b'"
+  sh "env | grep MYENV"
+}
+
+def myClosure(String a, CLosure b) {
+  withEnv(["MYENV=$a"]) {
+    b()
+  }
 }
 
 def call(Map args){
   echo "called by Map"
-  args.s.each{ genSec(it.a, it.b, isUnix()) }
+  myClosure('BY_MAP') {
+    args.s.each{ genSec(it.a, it.b, isUnix()) }
+  }
 }
 def call(List args){
   echo "called by list"
-  args.each{ genSec(it.a, it.b, isUnix()) }
+  myClosure('BY_LIST') {
+    args.each{ genSec(it.a, it.b, isUnix()) }
+  }
 }
