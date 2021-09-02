7f4c47f605daa3ffad3a69cc4eddc72f7c6aaa3e
Szymon Sobik <84793504+SS1823@users.noreply.github.com>
2021-09-02 21:26:51 +0200

    sdf

--- vars/getSec.groovy
+++ vars/getSec.groovy
@@ -1,12 +1,12 @@
-def genSec(String a, String b) {
+def genSec(String a, String b, def boo) {
   echo "a='$a' b='$b'"
 }
 
 def call(Map args){
   echo "called by Map"
-  args.s.each{ genSec(it.a, it.b) }
+  args.s.each{ genSec(it.a, it.b, isUnix()) }
 }
 def call(List args){
   echo "called by list"
-  args.each{ genSec(it.a, it.b) }
+  args.each{ genSec(it.a, it.b, isUnix()) }
 }
